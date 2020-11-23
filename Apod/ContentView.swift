07494aae68c502a40fa45bc897ad961852890dd4
Lucas Silva <lucasxvirtual@gmail.com>
2020-11-22 21:09:01 -0300

    Initial Commit

---
+++ Apod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apod
+//
+//  Created by Lucas Silva on 22/11/20.
+//  Copyright © 2020 Lucas Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
